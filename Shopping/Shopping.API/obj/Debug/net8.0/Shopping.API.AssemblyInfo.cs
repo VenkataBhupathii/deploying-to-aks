40399375ff8ffaf73c94b59357b3e553e461a8c5
venkatabhupathiii <bhupathi.mala@ensarsolutions.com>
2024-12-30 15:39:27 +0530

    clientpipeline

--- Shopping/Shopping.API/obj/Debug/net8.0/Shopping.API.AssemblyInfo.cs
+++ Shopping/Shopping.API/obj/Debug/net8.0/Shopping.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shopping.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+461552d4284dade98a28bcc9ba713d6c7c5d199f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a90b9fd35280da5d7cc406e4c3700d4f335dbccc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shopping.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shopping.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
